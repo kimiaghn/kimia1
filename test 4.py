6a54876015ffe0404322fa4b12d8cba63103b2a7
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-19 23:34:58 +0430

    home work starts

--- test 4.py
+++ test 4.py
@@ -1,1 +1,19 @@
 print("help")
+mass = int(input("enter mass"))
+weight = float(input("enter weight"))
+height = int(input("height"))
+bmi = float(mass / (weight * height))
+print(bmi)
+
+text_1 = """hello
+my 
+friend"""
+print(text_1)
+text_2 = "we are \ntesting \na lining method"
+print(text_2)
+
+a = 10
+b = 10
+c = a*b
+print(c)
+
