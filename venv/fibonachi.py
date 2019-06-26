75f2e6706cd58490d2c8cc1b2cf8839c5036adbe
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 00:03:15 +0430

    fibonachi homework

---
+++ venv/fibonachi.py
@@ -0,0 +1,33 @@
+# limit = int(input('enter limit'))
+# i = 0
+# n1 = 1
+# n2 = 1
+# fibonachi = 0
+# print(n1)
+# print(n2)
+# while i < limit :
+#     i = i + 1
+#     if i != limit :
+#         if i >= 2 :
+#             if fibonachi <= limit :
+#                  fibonachi = fibonachi + i
+#                  print(fibonachi)
+#
+
+limit = int(input('enter limit'))
+first = 1
+second = 1
+third = 0
+i =  0
+fibonachi = [1,1]
+print(first)
+print(second)
+while i <= limit :
+    i = i + 1
+    if i >= 2 :
+        third = first + second
+        first = second
+        second = third
+        print(third)
+        fibonachi.append(third)
+print(fibonachi)
