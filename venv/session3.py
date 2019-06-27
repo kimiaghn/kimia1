a5065f37bf7a5bc7d3b787e483c909c932b1a2f4
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 15:37:06 +0430

    list

--- venv/session3.py
+++ venv/session3.py
@@ -3,6 +3,8 @@
 #     print(i)
 
 sum = 0
+n = int(input('enter n'))
 for i in range (1,n+1) :
-    sum = float((1 / (i**2)) + sum)
+        sum = (1 / (i**2)) + sum
     print(i,sum)
+print((sum * 6)**0.5)
