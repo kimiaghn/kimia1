42db94d4fe6ac6cf404afa185c7de2dfeb49a295
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 17:35:13 +0430

    random

--- venv/Scripts/ASSCII table.py
+++ venv/Scripts/ASSCII table.py
@@ -1,5 +1,5 @@
-for i in range (90,122) :
-    num = ord[i]
-    alpha = chr[i]
+for i in range (0,255) :
+    num = int(ord[i])
+    alpha = str(chr[i])
     lst = [num, alpha]
     print('ASSCII', lst)
