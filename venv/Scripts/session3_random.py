4a8432aceb27943d9b126a629741ac9a6db29916
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 17:03:52 +0430

    random

--- venv/Scripts/session3_random.py
+++ venv/Scripts/session3_random.py
@@ -3,8 +3,12 @@ import random
 computer_rand = random.randint(1,10)
 t_1 = time.time()
 while True :
-    guessed_number = int(input('enter any number = '))
-    if guessed_number == computer_rand :
+    guessed_number = int(input('enter any number between 1 and 10 = '))
+    if guessed_number > computer_rand :
+        print('guess less')
+    elif guessed_number < computer_rand :
+        print('guess more')
+    elif guessed_number == computer_rand :
         print('you guessed right !')
         break
 
