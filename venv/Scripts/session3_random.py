975d7e5b0c4714362dc3373dbef118cf7568bfb0
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 16:55:03 +0430

    list

--- venv/Scripts/session3_random.py
+++ venv/Scripts/session3_random.py
@@ -1,5 +1,5 @@
 import random
-computer_rand = random.random(1,10)
+computer_rand = random.randint(1,10)
 while true :
     guessed_number = int(input('enter any number'))
     if guessed_number == computer_rand :
