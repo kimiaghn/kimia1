c79350e2d65db6b9cdebd30d44c041a3ee424bd8
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 14:41:21 +0430

    list

--- venv/homework-list.py
+++ venv/homework-list.py
@@ -1,7 +1,8 @@
-number = [ 10, 20, 30, 40, 50, 60 ]
+number = [ 10, 20, 30, 40, 30,  50, 60 ]
 print (number[2])
 number.append ( 1000 )
 print(number)
 number.insert(2,100000)
-number.copy()
+# number.extend()
+number.index(30,1,3)
 print(number)
--- venv/list.py
+++ venv/list.py
@@ -1,5 +1,8 @@
-lst = [1,2,3,4,5,6,7]
-lst.insert(1,15000)
-print(lst)
-delete = lst.pop(1)
-print(delete)
+# lst = [1,2,3,4,5,6,7]
+# lst.insert(1,15000)
+# print(lst)
+# delete = lst.pop(1)
+# print(delete)
+
+dictionary = {'singer':'Hoizer', 'song':'take me to church'}
+dictionary['singer']
