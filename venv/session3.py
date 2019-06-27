5e2726a533366fa1630eb3f38a57fea1f352a19d
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 16:05:17 +0430

    list

--- venv/session3.py
+++ venv/session3.py
@@ -2,10 +2,18 @@
 # for i in range(1,n+1) :
 #     print(i)
 
-sum_r = 0
-n = int(input('enter n'))
-for i in range (1,n+1) :
-        sum_r = (1 / (i**2)) + sum_r
-    print(i,sum_r)
-print('pi =')
-print((sum_r * 6)**0.5)
+# sum_r = 0
+# n = int(input('enter n'))
+# for i in range (1,n+1) :
+#         sum_r = (1 / (i**2)) + sum_r
+#     print(i,sum_r)
+# print('pi =')
+# print((sum_r * 6)**0.5)
+
+n = 1000
+zeta = 0
+i = 1
+while 3.1415 - zeta > 0.0001 :
+    zeta = zeta + (1 / i**2)
+    i = i + 1
+print(zeta)
