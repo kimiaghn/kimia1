f6f689d50bf2df6d163af25e23919a75088e71a1
kimiaghn <kimia.ghaseminasab@gmail.com>
2019-06-27 17:45:28 +0430

    random

--- venv/Scripts/ASSCII table.py
+++ venv/Scripts/ASSCII table.py
@@ -1,5 +1,10 @@
-for i in range (0,255) :
+# for i in range (0,255) :
+#   print( *[i, chr(i)])
 
-    alpha = chr[i]
-    lst = [i, alpha]
-    print('ASSCII', lst)
+
+columns = 8
+rows = 256 // columns
+for i in range(rows) :
+    for j in range(2) :
+        num = j * 2 + iprint(num , chr(num), end = '\t|\t')
+    print()
